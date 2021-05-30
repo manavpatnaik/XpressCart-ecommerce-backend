75acd0c659dccb5703c4463e253a87d60cbf1a5e
Manav Patnaik <manavpatnaik@hotmail.com>
2021-05-30 16:35:11 +0530

    User registration optmizations

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -3,10 +3,10 @@ const ErrorResponse = require("../utils/ErrorResponse");
 
 exports.register = async (req, res, next) => {
   const userEmail = await User.findOne({ email: req.body.email });
-  if (userEmail) return next(new ErrorResponse("User already registered", 400));
+  if (userEmail) return next(new ErrorResponse("Email already registered", 400));
 
-  const userName = await User.findOne({ email: req.body.username });
-  if (userName) return next(new ErrorResponse("User already registered", 400));
+  const userName = await User.findOne({ username: req.body.username });
+  if (userName) return next(new ErrorResponse("Username already registered", 400));
 
   const _user = new User(req.body);
   try {
@@ -15,6 +15,12 @@ exports.register = async (req, res, next) => {
       .status(201)
       .send({ success: true, message: "User created Successfully" });
   } catch (err) {
-    next("Something went wrong, User could not be registered", 500);
+    console.log(err.message);
+    next(
+      new ErrorResponse(
+        "Something went wrong, User could not be registered",
+        500
+      )
+    );
   }
 };

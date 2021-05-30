c71ac17048db28a508e6c2ad4a54276ee5628830
Manav Patnaik <manavpatnaik@hotmail.com>
2021-05-30 16:15:43 +0530

    Added custom error handling middleware

---
+++ src/middleware/error.js
@@ -0,0 +1,16 @@
+const ErrorResponse = require("../utils/ErrorResponse");
+
+const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+  error.message = err.message;
+
+  // Mongoose validation error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  res.statusCode(error.statusCode || 500).send({ Error: error.message });
+};
+
+module.exports = errorHandler;

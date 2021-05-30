979c1108ebec8a1ae480923586db99ee99e9b23c
Manav Patnaik <manavpatnaik@hotmail.com>
2021-05-30 16:06:35 +0530

    Added User model

---
+++ src/models/User.js
@@ -0,0 +1,68 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 20,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 20,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      index: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    contactNumber: {
+      type: String,
+    },
+    profilePicture: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  const password = this.password;
+  this.password = await bcrypt.hash(password, salt);
+});
+
+UserSchema.methods = {
+  authenticate: async function (password) {
+    return await bcrypt.compare(password, this.hash_password);
+  },
+};
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;

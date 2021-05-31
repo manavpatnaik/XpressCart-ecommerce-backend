d47ac9a2ba615ca4555b642c6c8e3c3b2662d930
Manav Patnaik <manavpatnaik@hotmail.com>
2021-05-31 17:01:15 +0530

    Updated Middleware

--- src/middleware/requireUser.js
+++ src/middleware/requireUser.js
@@ -1,8 +1,8 @@
 const ErrorResponse = require("../utils/ErrorResponse");
 
 const requireAdmin = (req, res, next) => {
-  if (req.user.role !== "admin") {
-    return next(new ErrorResponse("Access Denied - Must be an Admin", 400));
+  if (req.user.role !== "user") {
+    return next(new ErrorResponse("Access Denied - Must be a User", 400));
   }
   next();
 };

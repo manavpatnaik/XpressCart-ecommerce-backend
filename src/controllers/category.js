59402b51a4fe00bf4dd8bf7b6dadf5ffa287c032
Manav Patnaik <manavpatnaik@hotmail.com>
2021-05-31 17:14:03 +0530

    Created parent child relationship (nested structure) in getCategories

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -3,6 +3,29 @@ const Category = require("../models/Category");
 const ErrorResponse = require("../utils/ErrorResponse");
 const asyncHandler = require("../middleware/asyncHandler");
 
+const createCategories = (categories, parentId = null) => {
+  let nestedCategories = [];
+  let category;
+
+  // Creating levels of categories based in parentId
+  if (parentId == null) {
+    category = categories.filter((cat) => cat.parentId == null);
+  } else {
+    category = categories.filter((cat) => cat.parentId == parentId);
+  }
+
+  for (cat of category) {
+    nestedCategories.push({
+      _id: cat._id,
+      name: cat.name,
+      slug: cat.slug,
+      children: createCategories(categories, cat._id),
+    });
+  }
+
+  return nestedCategories;
+};
+
 exports.addCategory = asyncHandler(async (req, res, next) => {
   const cat = await Category.findOne({ name: req.body.name });
   if (cat) return next(new ErrorResponse("Category already exists", 400));
@@ -26,5 +49,9 @@ exports.addCategory = asyncHandler(async (req, res, next) => {
 
 exports.getCategories = asyncHandler(async (req, res, next) => {
   const categories = await Category.find();
-  res.status(200).send(categories);
+
+  // Create nested Structure
+  const nestedCategories = createCategories(categories);
+  res.status(200).send(nestedCategories);
+  // res.status(200).send(categories);
 });

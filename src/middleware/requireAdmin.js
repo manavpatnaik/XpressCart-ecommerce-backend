82e693ca179d690c31a1201bd71d1d385d3c2fdb
Manav Patnaik <manavpatnaik@hotmail.com>
2021-05-31 16:18:40 +0530

    Added admin middleware

---
+++ src/middleware/requireAdmin.js
@@ -0,0 +1,8 @@
+const ErrorResponse = require("../utils/ErrorResponse");
+
+const requireAdmin = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return next(new ErrorResponse("Access Denied", 400));
+  }
+  next();
+};

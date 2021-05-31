daf1dd1b17f58b27affa19c678fcae8cd451a837
Manav Patnaik <manavpatnaik@hotmail.com>
2021-05-31 11:49:58 +0530

    Added authentication middleware

---
+++ src/middleware/requireLogin.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const ErrorResponse = require("../utils/ErrorResponse");
+
+const requireLogin = (req, res, next) => {
+  if (!req.headers.authorization)
+    return next(new ErrorResponse("Token missing from headers", 400));
+
+  const token = req.headers.authorization.split(" ")[1];
+  let user;
+  try {
+    user = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    next(new ErrorResponse(err.message, 400));
+  }
+
+  req.user = user;
+  next();
+};
+
+module.exports = requireLogin;

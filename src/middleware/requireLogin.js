f72d652107a92b9446df6e64e2eb35278f64e2a1
Manav Patnaik <manavpatnaik@hotmail.com>
2021-06-01 10:05:22 +0530

    Updated error messages in error middleware

--- src/middleware/requireLogin.js
+++ src/middleware/requireLogin.js
@@ -3,14 +3,14 @@ const ErrorResponse = require("../utils/ErrorResponse");
 
 const requireLogin = (req, res, next) => {
   if (!req.headers.authorization)
-    return next(new ErrorResponse("Token missing from headers", 400));
+    return next(new ErrorResponse("Access Denied - Token required", 400));
 
   const token = req.headers.authorization.split(" ")[1];
   let user;
   try {
     user = jwt.verify(token, process.env.JWT_SECRET);
   } catch (err) {
-    next(new ErrorResponse("Invalid Token", 400));
+    next(new ErrorResponse("Access Denied - Invalid Token", 400));
   }
 
   req.user = user;

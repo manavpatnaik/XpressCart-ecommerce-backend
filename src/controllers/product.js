40100974bdebd2591e1bfc3371f60f9f3f181cec
Manav Patnaik <manavpatnaik@hotmail.com>
2021-06-01 11:14:55 +0530

    Added product creation API

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,7 +1,29 @@
 const asyncHandler = require("../middleware/asyncHandler");
 const Product = require("../models/Product");
 const shortId = require("shortid");
+const slugify = require("slugify");
 
 exports.createProduct = asyncHandler(async (req, res, next) => {
-  res.status(200).json({ file: req.files, body: req.body });
+  const { name, price, description, category, quantity,  } = req.body;
+
+  let productImages = [];
+  if (req.files.length > 0) {
+    productImages = req.files.map((file) => ({
+      img: file.filename,
+    }));
+  }
+
+  const _product = new Product({
+    name,
+    slug: slugify(name),
+    price,
+    description,
+    quantity,
+    productImages,
+    category,
+    createdBy: req.user.id,
+  });
+
+  const product = await _product.save();
+  res.status(200).json({ product });
 });
